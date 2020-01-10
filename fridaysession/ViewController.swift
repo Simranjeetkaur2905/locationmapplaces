5d8105187e5e0516224808778665969ec0b7388d
simranjeetkaur <chakkalsimmi@gmail.com>
2020-01-10 15:18:32 -0500

    Initial Commit

---
+++ fridaysession/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fridaysession
+//
+//  Created by Simran Chakkal on 2020-01-10.
+//  Copyright © 2020 Simran Chakkal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
